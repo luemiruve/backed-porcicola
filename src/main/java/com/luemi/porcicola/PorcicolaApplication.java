b93af7340a15944d7f512a9f7d0290601731aa72
Luis Emilio Ruiz Vega <luemiruve@gmail.com>
2026-04-10 12:04:09 -0600

    feat: estructura inicial del backend porcicola

---
+++ src/main/java/com/luemi/porcicola/PorcicolaApplication.java
@@ -0,0 +1,13 @@
+package com.luemi.porcicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorcicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorcicolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luemi/porcicola/PorcicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luemi.porcicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorcicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
