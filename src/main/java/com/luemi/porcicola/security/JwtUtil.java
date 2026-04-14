113837eb48908aae45aaffa2b9cb4e4551ad8fee
Luis Emilio Ruiz Vega <luemiruve@gmail.com>
2026-04-14 17:59:20 -0600

    feat: crear el archivo JwtUtil generador de tokens

---
+++ src/main/java/com/luemi/porcicola/security/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.luemi.porcicola.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email, String rol, Integer idGranja) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("rol", rol)
+                .claim("idGranja", idGranja)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public String extractRol(String token) {
+        return getClaims(token).get("rol", String.class);
+    }
+
+    public Integer extractIdGranja(String token) {
+        return getClaims(token).get("idGranja", Integer.class);
+    }
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
